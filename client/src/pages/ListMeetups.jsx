58570212798db9cac6ef2bb172aebadf8ba27ab7
Matteo <matteo.stella.web@gmail.com>
2024-01-15 21:44:28 +0100

    map test added

--- client/src/pages/ListMeetups.jsx
+++ client/src/pages/ListMeetups.jsx
@@ -1,10 +1,40 @@
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const ListMeetups = () => {
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+  const newCenter = {
+    lat: -4.745,
+    lng: -39.523,
+  };
+  // const [map, setmap] = useState(null);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_MAPS_TOKEN,
+  });
   return (
-    <div>
-      Meetups List
-    </div>
-  )
-}
+    <div className="">
+      <h1>meetups map</h1>
+      {isLoaded && (
+        <GoogleMap
+          onClick={(e) => console.log(e.latLng.lat(), e.latLng.lng())}
+          mapContainerStyle={{ height: "100vh", width: "100%" }}
+          zoom={10}
+          center={center}
+          
+        >
+          
+          <Marker 
+        
+          position={center}></Marker>
+          <Marker position={newCenter}></Marker>
 
-export default ListMeetups
+        </GoogleMap>
+      )}
+    </div>
+  );
+};
+
+export default ListMeetups;
