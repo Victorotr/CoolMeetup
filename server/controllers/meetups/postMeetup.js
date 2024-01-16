214932cdeeca92b882d8a820b35aaf88cbf511f9
Victor Otero <victoroterovidal@gmail.com>
2024-01-16 13:25:25 +0100

    ADD: new endpoint: add endpoint postMeetup

--- server/controllers/meetups/postMeetup.js
+++ server/controllers/meetups/postMeetup.js
@@ -1,18 +1,64 @@
+// Functions requires ↓
+
+import { generateError } from '../../services/generateError.js';
+
 // Querie require ↓
 
 import { postMeetupQuery } from '../../db/meetupQueries/postMeetupQuery.js';
 
+// Joi require ↓
+
+import meetupJoi from '../../jois/meetupSchemas.cjs';
+const { newMeetupJoi } = meetupJoi;
 // Controller ↓
+
 export const postMeetup = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const {
+      meetup_title,
+      meetup_description,
+      meetup_province,
+      meetup_town,
+      meetup_datetime,
+    } = req.body;
 
-    // Query: get meetup by id
-    const postMeetup = await postMeetupQuery(id);
+    const { id } = req.userInfo;
+
+    // Joi validation
+    const schema = newMeetupJoi;
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+      throw generateError(validation.error.message, 401);
+    }
+
+    const meetupDatetime = new Date(meetup_datetime);
+    const date = new Date();
+
+    if (date.getTime() > meetupDatetime.getTime()) {
+      throw generateError(
+        'La fecha del meetup no puede ser anterior a hoy',
+        401
+      );
+    }
+
+    // Query: Create offer
+    const meetupId = await postMeetupQuery(
+      id,
+      meetup_title,
+      meetup_description,
+      meetup_province,
+      meetup_town,
+      meetup_datetime
+    );
+
     // Res.send
-    res.status(200).send({
+    res.status(201).send({
       status: 'ok',
-      data: postMeetup,
+      message: `Meetup con id (${meetupId}) creada con éxito`,
+      data: {
+        id: meetupId,
+      },
     });
   } catch (error) {
     next(error);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,7 @@ import { Ping } from '../controllers/cookies/Ping.js';
 import { OnRefreshCookies } from '../controllers/cookies/OnRefreshCookies.js';
 import { getAllMeetups } from '../controllers/meetups/getAllMeetups.js';
 import { getSingleMeetupDetails } from '../controllers/meetups/getSingleMeetupDetails.js';
+import { postMeetup } from '../controllers/meetups/postMeetup.js';
 import { SignIn } from '../controllers/signin/signIn.js';
 import { LogOut } from '../controllers/cookies/LogOut.js';
 import { isLogged } from '../controllers/signin/isLogged.js';
@@ -12,18 +13,17 @@ const router = Router();
 // Rutas de Meetups
 router.get('/', getAllMeetups);
 
-router.get('/islogged',isLogged);
-
 router.get('/meetup/:id', getSingleMeetupDetails);
 
+router.post('/meetup', postMeetup);
 
-router.post('/signin',SignIn);
+router.get('/islogged', isLogged);
 
-router.get('/logout',LogOut);
+router.post('/signin', SignIn);
+
+router.get('/logout', LogOut);
 // router.post('/singUp/:meetupId', singUpForMeetup);
 
-
-// router.post('/meetup', postMeetup);
 // router.post('/singUp/:meetupId', singUpForMeetup);
 
 // Rutas de Usuarios
