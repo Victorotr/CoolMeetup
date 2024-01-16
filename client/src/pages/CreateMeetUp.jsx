018cf0a2b4b53ddbf864355ff898ae4c462b5778
Matteo <matteo.stella.web@gmail.com>
2024-01-16 13:01:47 +0100

    Create meetup Google maps address selection added

--- client/src/pages/CreateMeetUp.jsx
+++ client/src/pages/CreateMeetUp.jsx
@@ -1,83 +1,144 @@
-import  { useState } from "react";
-import { GoogleMap, Marker, InfoWindow,useJsApiLoader } from "@react-google-maps/api";
+import {useState} from "react";
+import {
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  useJsApiLoader,
+} from "@react-google-maps/api";
+import { IoMdClose } from "react-icons/io";
+import { MdSearch } from "react-icons/md";
 
 const Map = () => {
   const [map, setMap] = useState(null);
-  const [center, setCenter] = useState({ lat: 40.4165000, lng: -3.7025600});
+  const [center, setCenter] = useState({ lat: 40.4165, lng: -3.70256 });
   const [address, setAddress] = useState("");
   const [coordsResult, setCoordsResult] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
+  const [queryResults, setqueryResults] = useState(null);
+  const [selected, setselected] = useState({ isSelected: false });
+  
 
   const onMapLoad = (mapInstance) => {
     setMap(mapInstance);
   };
-
-  const handleSearch = () => {
-    if (!map || !address) return;
-
-    const geocoder = new window.google.maps.Geocoder();
-
-    geocoder.geocode({ address: address }, (results, status) => {
-      console.log('results',results)
-      if (status === window.google.maps.GeocoderStatus.OK) {
-        setCenter(results[0].geometry.location);
-        setCoordsResult([results[0].geometry.location]);
-      } else {
-        console.error("Geocode was not successful for the following reason:", status);
-      }
-    });
+  const handleCoords = (coords) => {
+    setCoordsResult([coords]);
   };
-
   const handleMarkerClick = (marker) => {
     setSelectedMarker(marker);
-  };
 
+  };
   const handleInfoWindowClose = () => {
     setSelectedMarker(null);
   };
- const { isLoaded } = useJsApiLoader({
+  const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_MAPS_TOKEN,
   });
+
+  const handleSearch = () => {
+    if (!map || !address || selected.isSelected) return;
+
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode({ address: address }, (results, status) => {
+      if (status === window.google.maps.GeocoderStatus.OK) {
+        setqueryResults(results);
+      } else {
+        setqueryResults(null);
+      }
+    });
+  };
   return (
-  
-      <div>
-        <div>
-          <input
-            type="text"
-            placeholder="Ingrese la dirección"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <button onClick={handleSearch}>Buscar</button>
+    <section className="w-full max-w-4xl mx-auto py-10">
+      <h1 className="font-Lora text-xl px-2">Crear Meetup</h1>
+      <div className="mx-1 px-2 rounded-md my-10 flex items-center font-medium bg-zinc-50 relative border border-zinc-900/10 justify-between py-3">
+        <input
+          type="text"
+          placeholder="Busca una dirección"
+          onKeyDown={(e)=>{if(e.key === 'Enter'){handleSearch()}}}
+          value={address}
+          disabled={selected.isSelected}
+          className="w-full h-full py-3 px-2 placeholder:text-zinc-900/60 focus:outline-none bg-transparent"
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <div className="flex items-center">
+          <span onClick={handleSearch} 
+          className={`${selected.isSelected && 'hidden'}  hover:scale-110 transition-all`}
+         >
+            <MdSearch  className="text-zinc-900/70" size={25} />
+          </span>
+          <span
+            onClick={() => {
+              setqueryResults(null);
+              setCoordsResult([]);
+              setAddress("");
+              setselected({ isSelected: false });
+            }}
+            className={`${!address.length && 'hidden'} hover:scale-110 transition-all`}
+          >
+            <IoMdClose 
+
+            className="text-zinc-900/70" size={30} />
+          </span>
         </div>
-        {isLoaded && 
-        <div className="w-full h-96">
-          <GoogleMap
-          center={center}
-          zoom={8}
-          onLoad={onMapLoad}
-          mapContainerStyle={{ height: "100%", width:"100%" ,border:0 }}
+        <div
+          className={`${
+            queryResults ? "block" : "hidden"
+          } w-full bg-zinc-50 absolute top-16 left-0 z-40 `}
         >
-          {coordsResult.map((coords, i) => (
-            <Marker
-              key={i}
-              position={coords}
-              onClick={() => handleMarkerClick(coords)}
-            >
-              {selectedMarker === coords && (
-                <InfoWindow onCloseClick={handleInfoWindowClose} position={coords}>
-                  <div>
-                    <span>{address}</span>
-                  </div>
-                </InfoWindow>
-              )}
-            </Marker>
-          ))}
-        </GoogleMap></div>}
-      
+          <ul className={`${queryResults ? "block" : "hidden"}`}>
+            {queryResults &&
+              queryResults.map((item) => (
+                <li
+                  onClick={() => {
+                    setqueryResults(null);
+                    handleCoords(item.geometry.location);
+                    setCenter(item.geometry.location);
+                    setAddress(item.formatted_address);
+                    setselected({ isSelected: true });
+                  }}
+                  key={item.place_id}
+                  className="border p-3 py-4 hover:bg-blue-500/20 transition-all hover:cursor-pointer"
+                >
+                  {item.formatted_address}
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-   
+      {isLoaded && (
+        <div className="w-full h-[500px]">
+          <GoogleMap
+            center={center}
+            zoom={coordsResult.length ? 15 : 5}
+            options={{ disableDefaultUI: true }}
+            onLoad={onMapLoad}
+            mapContainerStyle={{ height: "100%", width: "100%", border: 0 }}
+          >
+            {coordsResult.map((coords, i) => (
+              <Marker
+                key={i}
+                position={coords}
+                onClick={() => {
+                  handleMarkerClick(coords);
+                }}
+              >
+                {selectedMarker === coords && (
+                  <InfoWindow
+                    onCloseClick={handleInfoWindowClose}
+                    position={coords}
+                  >
+                    <div className="p-2">
+                      <span>{address}</span>
+                    </div>
+                  </InfoWindow>
+                )}
+              </Marker>
+            ))}
+          </GoogleMap>
+        </div>
+      )}
+    </section>
   );
 };
 
