2a374a7ffe265a0ac309fdaaafa6dca4cae9320b
Matteo <matteo.stella.web@gmail.com>
2024-01-17 15:13:06 +0100

    regular login added and google login added user picture added

--- server/controllers/signin/signIn.js
+++ server/controllers/signin/signIn.js
@@ -21,34 +21,38 @@ export const SignIn = async (req, res) => {
     const expiration = 1000 * 60 * 60 * 24 * 28;
     const sessionOpen = req.body.checked;
     const connection = await getConnection();
-    
+
     try {
         const { email, pwd } = req.body;
-        const [user] = await connection.query('SELECT id_user as id,user_name as username,user_password as pwd,active  FROM users WHERE user_email =?', [email]);
+        const [user] = await connection.query('SELECT id_user as id,user_name as username,user_password as pwd,active,google_registered,user_picture FROM users WHERE user_email =?', [email]);
         console.log(user)
         if (!user.length) { return res.status(403).send({ message: 'Usuario no encontrado' }) }
         const dbpwd = user[0].pwd;
-       
-        const compare = await comparePasswords(pwd,dbpwd);
-        if(!compare){ return res.status(403).send({ message: 'Usuario o contraseña incorectos' })}
-         if(!user[0].active){ return res.status(403).send({ message: 'Usuario no activado' })}
+        if (user[0].google_registered){
+            console.log('google registered') 
+            return res.status(403).send({ message: 'Usuario registrado con Google, utiliza el botón de Acceso con Google  para continuar' })
+         }
+        const compare = await comparePasswords(pwd, dbpwd);
+        if (!compare) { return res.status(403).send({ message: 'Usuario o contraseña incorectos' }) }
+        if (!user[0].active) { return res.status(403).send({ message: 'Usuario no activado' }) }
         const info = {
             id: user[0].id,
-            username:user[0].username,
-            email:user[0].email
-          };
-          const token = jwt.sign(info, process.env.SECRET_TOKEN, {
+            username: user[0].username,
+            email: user[0].email,
+            avatar:user[0].user_picture || null
+        };
+        const token = jwt.sign(info, process.env.SECRET_TOKEN, {
             expiresIn: '28d',
-          });
-        sessionOpen && res.cookie('user_token', { token:token },{
+        });
+        sessionOpen && res.cookie('user_token', { token: token }, {
             maxAge: expiration,
             httpOnly: true,
-            secure: false, 
+            secure: false,
             sameSite: 'lax',
-            path: '/' 
+            path: '/'
         });
-        res.status(200).send({ status: 'Logged', user: info});
-       
+        res.status(200).send({ status: 'Logged', user: info });
+
     } catch (error) {
         res.clearCookie('user_token')
         res.status(403).send({ message: 'Problemas con el servidor, por favor intentelo más tarde' })
