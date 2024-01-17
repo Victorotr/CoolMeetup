23b18e954bd27a18557f31c9ed98141e2e0ddc9e
bocanegrapps <hola@informaticabocanegra.com>
2024-01-17 13:40:54 +0100

    Merge ramas Matteo

--- server/Controllers/users/loginRegisterWithGoogle.js
+++ server/Controllers/users/loginRegisterWithGoogle.js
@@ -1,33 +1,31 @@
-import {getConnection} from "../../db/ConnectionDB.js"
+import { getConnection } from '../../db/ConnectionDB.js';
 
 const loginRegisterWithGoogle = async (req, res, next) => {
   let connect;
   try {
     let { mail, name } = req.body;
-       
+
     connect = await getConnection();
-    
+
     const [userExist] = await connect.query(
-      `SELECT id_user, nombre_usuario FROM users WHERE email_usuario=?`,
+      `SELECT id_user, user_name FROM users WHERE user_email=?`,
       [mail]
     );
 
     if (userExist.length > 0) {
       res.status(200).send({
-        status: "ok",
-        message: "Bienvenido de nuevo "+userExist[0].nombre_usuario,
+        status: 'ok',
+        message: 'Bienvenido de nuevo ' + userExist[0].nombre_usuario,
       });
-    }
-    else
-    {
+    } else {
       const [users] = await connect.query(
-        `INSERT INTO users (email_usuario, nombre_usuario, active) VALUES (?,?,1)`,
+        `INSERT INTO users (user_email, user_name, active) VALUES (?,?,1)`,
         [mail, name]
       );
       res.status(200).send({
-        status: "ok",
-        message: "Estamos encantados de tenerte con nosotros "+name,
-      });  
+        status: 'ok',
+        message: 'Estamos encantados de tenerte con nosotros ' + name,
+      });
     }
   } catch (error) {
     console.log(error);
