2416f76b41a5b26007d109c8bbbb09002d494c41
bocanegraapps <informaticabocanegra@gmail.com>
2024-01-21 18:27:19 +0100

    casa 21-1

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // Express cors
 app.use(cors({
     origin:[process.env.CLIENT_URL,'http://localhost:5173'],
+    origin:[process.env.CLIENT_URL,'http://172.16.183.28:5173'],
     credentials: true,
   })
 );
