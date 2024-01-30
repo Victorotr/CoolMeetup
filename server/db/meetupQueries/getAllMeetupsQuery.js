4a29f7ecf7056b73af60b7025a70d9d020d9af4c
Matteo <matteo.stella.web@gmail.com>
2024-01-30 13:46:49 +0100

    added filters querys

--- server/db/meetupQueries/getAllMeetupsQuery.js
+++ server/db/meetupQueries/getAllMeetupsQuery.js
@@ -3,14 +3,16 @@ import { getConnection } from '../ConnectionDB.js';
 export const getAllMeetupsQuery = async (filters) => {
   let connection;
   const mysqlDate = filters.date.split("/").reverse().join("-");
-  console.log(mysqlDate);
+  
   const theme = filters.tematica;
   const province = filters.provincia;
+  const order = filters.order === 'Fecha' ? 'meetup_datetime ASC' : filters.order === 'Asistentes' ? 'assistants DESC' : 'meetup_datetime ASC'
+  console.log('order', order)
   let themeFilter = "";
   let provinceFilter = "";
+  
   theme !== "Todas" ? (themeFilter = " AND meetups.meetup_theme = '" + theme + "'") : (themeFilter = "");
   province !== "Todas" ? (provinceFilter = " AND meetups.meetup_town = '" + province + "'") : (provinceFilter = "");
-
   try {
     connection = await getConnection();
     const [allMeetups] = await connection.query(
@@ -20,24 +22,18 @@ export const getAllMeetupsQuery = async (filters) => {
       JSON_OBJECT('user_id', users.id_user, 'username', users.user_name, 'avatar', users.picture_url) 
       as main_user_details,
       COUNT(users_meetups.id_user) as assistants 
-    FROM meetups
+    FROM meetups 
     LEFT JOIN users ON meetups.id_main_user = users.id_user
     LEFT JOIN users_meetups ON users_meetups.id_meetup = meetups.id_meetup 
     WHERE meetups.meetup_datetime >= ? ${themeFilter} ${provinceFilter} 
-    GROUP BY meetups.id_meetup;
+    GROUP BY meetups.id_meetup ORDER BY ${order};
    `,
       [mysqlDate]
     );
 
-  
-    const parsedArray = allMeetups.map((item)=>{return {...item,main_user_details: JSON.parse(item.main_user_details)}});
-    
+    const parsedArray = allMeetups.map((item) => { return { ...item, main_user_details: JSON.parse(item.main_user_details) } });
 
-    allMeetups.sort((a, b) => {
-      return b.meetup_datetime - a.meetup_datetime;
-    });
-
-    return parsedArray;
+    return parsedArray || null;
   } catch (error) {
     console.error(error);
   } finally {
