0a18d4e22cba7a60b0bc3f4004ba4144148faf6d
Matteo <matteo.stella.web@gmail.com>
2024-02-05 00:18:03 +0100

    update

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Express cors
 app.use(cors({
-    origin:[process.env.CLIENT_URL,'http://localhost:5173'],
+    origin:[process.env.CLIENT_URL,'http://localhost:5173',''],
     
     credentials: true,
   })
