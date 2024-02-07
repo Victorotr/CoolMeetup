dccb37cdc45bf9a5f06f094884fa4708e9abecb0
Matteo <matteo.stella.web@gmail.com>
2024-02-07 17:35:13 +0100

    query debugging

--- server/db/meetupQueries/getSingleMeetupDetailsQuery.js
+++ server/db/meetupQueries/getSingleMeetupDetailsQuery.js
@@ -14,28 +14,29 @@ export const getSingleMeetupDetailsQuery = async (id) => {
       SELECT meetups.*, 
       users.id_user, 
       users.user_name, 
-      users.picture_url,
+      COALESCE(users.picture_url, 'URL_predeterminada') as picture_url,
       CONCAT('[', GROUP_CONCAT(
           CONCAT(
               '{"id_user": ', users_meetups.id_user,
               ', "user_name": "', u.user_name,
-              '", "picture_url": "', u.picture_url, '"}'
+              '", "picture_url": "', COALESCE(u.picture_url, 'URL_predeterminada'), '"}'
           )
       ), ']') AS assistants
       FROM meetups 
-      INNER JOIN users ON meetups.id_main_user = users.id_user 
+      LEFT JOIN users ON meetups.id_main_user = users.id_user 
       LEFT JOIN users_meetups ON users_meetups.id_meetup = meetups.id_meetup 
       LEFT JOIN users u ON users_meetups.id_user = u.id_user
       WHERE meetups.id_meetup = ?
-      GROUP BY meetups.id_meetup, users.id_user, users.user_name, users.picture_url;
+      GROUP BY meetups.id_meetup, users.id_user, users.user_name;
+
     `,
       [id]
     );
     if (meetupDetails.length) {
       console.log(meetupDetails[0])
       meetupDetails[0].assistants = JSON.parse(meetupDetails[0].assistants)
-   
-      
+
+
       return meetupDetails[0];
     }
     return null
