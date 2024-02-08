ab5e7a12e32fdcac54f93ef153d618dbd7ab756e
Victor Otero <victoroterovidal@gmail.com>
2024-02-08 11:35:27 +0100

    REFACTOR: query attendees

--- server/db/meetupQueries/getUserMeetupsAttendeesQuery.js
+++ server/db/meetupQueries/getUserMeetupsAttendeesQuery.js
@@ -11,22 +11,23 @@ export const getUserMeetupsAttendeesQuery = async (id) => {
       meetups.*,
       JSON_OBJECT('user_id', users.id_user, 'username', users.user_name, 'avatar', users.picture_url) as main_user_details,
       COALESCE(assistant_counts.assistants_count, 0) as assistants 
-  FROM meetups 
-  LEFT JOIN users ON meetups.id_main_user = users.id_user
-  LEFT JOIN (
+      FROM meetups 
+      LEFT JOIN users ON meetups.id_main_user = users.id_user
+      LEFT JOIN (
       SELECT id_meetup, COUNT(id_user) as assistants_count
       FROM users_meetups
       GROUP BY id_meetup
-  ) as assistant_counts ON meetups.id_meetup = assistant_counts.id_meetup
-  LEFT JOIN users_meetups ON users_meetups.id_meetup = meetups.id_meetup
-  WHERE users_meetups.id_user = ? OR users_meetups.id_user IS NULL
-  GROUP BY meetups.id_meetup;
-  `,
-
+      ) 
+      as assistant_counts ON meetups.id_meetup = assistant_counts.id_meetup
+      LEFT JOIN users_meetups ON users_meetups.id_meetup = meetups.id_meetup
+      WHERE users_meetups.id_user = ? 
+      GROUP BY meetups.id_meetup`,
       [id]
     );
-    console.log('getuserMettupsAtendees',userMeetupsAttendees)
-    const parsedArray = userMeetupsAttendees.map((item) => { return { ...item, main_user_details: JSON.parse(item.main_user_details) } });
+    console.log('getuserMettupsAtendees', userMeetupsAttendees);
+    const parsedArray = userMeetupsAttendees.map((item) => {
+      return { ...item, main_user_details: JSON.parse(item.main_user_details) };
+    });
 
     return parsedArray || null;
   } catch (error) {
