e774eb07a2b122bcfa7a7bac2ef9648b878290c4
bocanegrapps <hola@informaticabocanegra.com>
2024-02-08 17:25:07 +0100

    update mario branch

--- client/src/Cards/MeetupCard.tsx
+++ client/src/Cards/MeetupCard.tsx
@@ -19,71 +19,78 @@ const MeetupCard = ({ meetup }) => {
         alt=""
       />
       <div className="p-1 ">
-      <div className="p-2 h-40  w-full flex flex-col justify-between">
-        <div className="max-h-32 overflow-hidden">
-          <h4 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
-            {meetup.meetup_title}
-          </h4>
+        <div className="p-2 h-40  w-full flex flex-col justify-between">
+          <div className="max-h-32 overflow-hidden">
+            <h4 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+              {meetup.meetup_title}
+            </h4>
 
-          <p className="  overflow-ellipsis h-16 font-normal text-gray-700 dark:text-gray-400">
-            {meetup.meetup_description}
+            <p className="  overflow-ellipsis h-16 font-normal text-gray-700 dark:text-gray-400">
+              {meetup.meetup_description}
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <a
+              href={`/meetups/details/${meetup.id_meetup}`}
+              className="inline-flex items-center gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Más detalles
+            </a>
+            <a
+              href={`#`}
+              className="inline-flex float-right items-center px-3 py-2 text-sm font-medium text-center text-white bg-amber-500 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Me apunto!
+            </a>
+          </div>
+        </div>
+        <div className="px-2 py-2">
+          <p className=" flex  font-medium gap-7">
+            <span className="flex items-center gap-1">
+              <CiCalendar
+                className="text-zinc-900/80"
+                style={{ strokeWidth: "1.5px" }}
+              />{" "}
+              {moment(meetup.meetup_datetime).format("DD/MM/YY")}
+            </span>
+            <span className="flex items-center gap-1">
+              <CiClock1
+                className="text-zinc-900/80"
+                style={{ strokeWidth: "1.5px" }}
+              />
+              {moment(meetup.meetup_datetime, "HH:mm:ss").format("HH:mm")}
+            </span>
           </p>
         </div>
-        <div className="flex gap-2">
-          <a
-            href={`/meetup/details/${meetup.id_meetup}`}
-            className="inline-flex items-center gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Más detalles
-          </a>
-          <a
-            href={`#`}
-            className="inline-flex float-right items-center px-3 py-2 text-sm font-medium text-center text-white bg-amber-500 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Me apunto!
-          </a>
-        </div>
-      </div>
-      <div className="px-2 py-2">
-        <p className=" flex  font-medium gap-7">
-          <span className="flex items-center gap-1">
-            <CiCalendar className="text-zinc-900/80" style={{strokeWidth:'1.5px'}}  /> {moment(meetup.meetup_datetime).format("DD/MM/YY")}
-          </span>
-          <span className="flex items-center gap-1">
-            <CiClock1 className="text-zinc-900/80" style={{strokeWidth:'1.5px'}} />
-            {moment(meetup.meetup_datetime, "HH:mm:ss").format("HH:mm")}
-          </span>
-        </p>
-      </div>
 
-      <div className="flex justify-between items-center px-2">
-        <span className="">
-          {meetup.meetup_attendees === null ? (
-            <span className="border rounded-full px-2 py-1 shadow-md bg-green-500/50 font-medium">
-              Sé el primero en asistir!
-            </span>
-          ) : (
-            <span>
-              {meetup.meetup_attendees}{" "}
-              {`Asistirá` + (meetup.meetup_attendees > 1 ? "n" : "")}
-            </span>
-          )}
-        </span>
-        <div
-          className="flex items-center justify-end gap-1 p-2 cursor-pointer"
-          onClick={(e) => {
-            e.stopPropagation();
-            navigate(`/user/details/${meetup.id_main_user}`);
-          }}
-        >
-          <img
-            className="w-7 h-7 object-cover rounded-full"
-            src={meetup.picture_url || nouserpicure}
-            alt="user picture"
-          />
-          <p className="font-medium text-md">{meetup.user_name}</p>
+        <div className="flex justify-between items-center px-2">
+          <span className="">
+            {meetup.meetup_attendees === null ? (
+              <span className="border rounded-full px-2 py-1 shadow-md bg-green-500/50 font-medium">
+                Sé el primero en asistir!
+              </span>
+            ) : (
+              <span>
+                {meetup.meetup_attendees}{" "}
+                {`Asistirá` + (meetup.meetup_attendees > 1 ? "n" : "")}
+              </span>
+            )}
+          </span>
+          <div
+            className="flex items-center justify-end gap-1 p-2 cursor-pointer"
+            onClick={(e) => {
+              e.stopPropagation();
+              navigate(`/user/details/${meetup.id_main_user}`);
+            }}
+          >
+            <img
+              className="w-7 h-7 object-cover rounded-full"
+              src={meetup.picture_url || nouserpicure}
+              alt="user picture"
+            />
+            <p className="font-medium text-md">{meetup.user_name}</p>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
